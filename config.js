144c0f19480e78b2e65a435dc44b6c7b5a05def7
Saiganesh079 <150430634+Saiganesh079@users.noreply.github.com>
2025-08-24 13:30:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.__SUPABASE__ = {
-  url: 'https://YOUR-PROJECT.supabase.co',
-  anonKey: 'YOUR-PUBLIC-ANON-KEY'
+  url: 'https://viirftaxwpivemmhwmlk.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZpaXJmdGF4d3BpdmVtbWh3bWxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYwMjE2NzEsImV4cCI6MjA3MTU5NzY3MX0.P5ihHje5h08MiG3FMNzZeTBELOMMM9Iq7zh0_gM1_TY'
 };
